1de875e677725bcd6cc3cca4be6b23ef193691bf
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2020-12-03 23:52:51 -0300

    12 - 40. Coluna e Textos

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,26 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: 'Contador de Pessoas',
-    home: Container(
-      color: Colors.white,
+    home: Column(
+      // crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Pessoas: 0',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'Pode Entrar!',
+          style: TextStyle(
+            color: Colors.white,
+            fontStyle: FontStyle.italic,
+            fontSize: 30.0,
+          ),
+        ),
+      ],
     ),
   ));
 }
