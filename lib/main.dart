6a9d818c168bcd4b39e60ed78df7ed4fab86d564
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2020-12-06 17:27:07 -0300

    12 - 43. Stateless e Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,43 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: 'Contador de Pessoas',
-    home: Stack(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _infoText = 'Pode Entrar!';
+
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+
+      if(_people < 0) {
+        _infoText = 'Mundo invertido?!';
+      } else if(_people <= 10) {
+        _infoText = 'Pode Entrar!';
+      } else {
+        _infoText = 'Lotado!';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
       children: [
         Image.asset('images/restaurant.jpg', fit: BoxFit.cover, height: 1000.0),
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Pessoas: 0',
+              'Pessoas: $_people',
               style: TextStyle(
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
@@ -29,7 +58,9 @@ void main() {
                         color: Colors.white,
                       ),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      _changePeople(1);
+                    },
                   ),
                 ),
                 Padding(
@@ -42,13 +73,15 @@ void main() {
                         color: Colors.white,
                       ),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
                   ),
                 ),
               ],
             ),
             Text(
-              'Pode Entrar!',
+              _infoText,
               style: TextStyle(
                 color: Colors.white,
                 fontStyle: FontStyle.italic,
@@ -58,6 +91,6 @@ void main() {
           ],
         )
       ],
-    ),
-  ));
+    );
+  }
 }
